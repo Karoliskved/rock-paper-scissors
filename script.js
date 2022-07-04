f7255a85e0674b3e8991ad9624dc2ca7570739e1
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-04 15:44:37 +0300

    create computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test")
+//console.log("test")
+function computerPlay(){
+    let rndNumber=Math.floor(Math.random()*3);
+    console.log(rndNumber);
+    switch(rndNumber){
+        case 0:
+            cmpChoice="Rock";
+            break;
+        case 1:
+            cmpChoice="Paper";
+            break;
+        case 2:
+            cmpChoice="scissors";
+            break;
+    }
+    //console.log(cmpChoice)
+
+}
+computerPlay()
