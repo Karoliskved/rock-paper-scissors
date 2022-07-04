78a55102c757a59cfacce8f4bfeadbd0d23f0bab
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-04 16:01:11 +0300

    create oneRound function

--- script.js
+++ script.js
@@ -4,16 +4,54 @@ function computerPlay(){
     console.log(rndNumber);
     switch(rndNumber){
         case 0:
-            cmpChoice="Rock";
+            cmpChoice="ROCK";
             break;
         case 1:
-            cmpChoice="Paper";
+            cmpChoice="PAPER";
             break;
         case 2:
-            cmpChoice="scissors";
+            cmpChoice="SCISSORS";
             break;
     }
     //console.log(cmpChoice)
+    return cmpChoice;
+
+}
+function oneRound(playerSelection, computerSelection){
+    computerSelection=computerPlay();
+    switch (playerSelection.toUpperCase()){
+        case "ROCK":
+            switch (computerSelection){
+                case "PAPER":
+                    return "You lose"
+                    break;
+                case "SCISSORS":
+                    return "You win"
+                    break;
+            }
+            break;
+        case "PAPER":
+            switch (computerSelection){
+                case "SCISSORS":
+                    return "You lose"
+                    break;
+                case "ROCK":
+                    return "You win"
+                    break;
+            }
+            break;
+        case "SCISSORS":
+            switch (computerSelection){
+                case "ROCK":
+                    return "You lose"
+                    break;
+                case "PAPER":
+                    return "You win"
+                    break;
+            }
+            break;
+    }
+
 
 }
 computerPlay()
